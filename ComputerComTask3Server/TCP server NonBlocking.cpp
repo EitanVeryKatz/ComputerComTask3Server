d5a6903a571823ff6bed95f43b0bd7cbb76e1f2b
EitanVeryKatz <eitanka@mta.ac.il>
2025-06-10 23:39:07 +0300

    removed unnecessary comments

--- ComputerComTask3Server/TCP server NonBlocking.cpp
+++ ComputerComTask3Server/TCP server NonBlocking.cpp
@@ -29,40 +29,17 @@ int socketsCount = 0;
 void main() 
 {
     // Initialize Winsock (Windows Sockets).
-
-	// Create a WSADATA object called wsaData.
-    // The WSADATA structure contains information about the Windows 
-	// Sockets implementation.
 	WSAData wsaData; 
-    	
-	// Call WSAStartup and return its value as an integer and check for errors.
-	// The WSAStartup function initiates the use of WS2_32.DLL by a process.
-	// First parameter is the version number 2.2.
-	// The WSACleanup function destructs the use of WS2_32.DLL by a process.
+ 
 	if (NO_ERROR != WSAStartup(MAKEWORD(2,2), &wsaData))
 	{
         cout<<"Http Server: Error at WSAStartup()\n";
 		return;
 	}
 
-	// Server side:
-	// Create and bind a socket to an internet address.
-	// Listen through the socket for incoming connections.
-
-    // After initialization, a SOCKET object is ready to be instantiated.
-	
-	// Create a SOCKET object called listenSocket. 
-	// For this application:	use the Internet address family (AF_INET), 
-	//							streaming sockets (SOCK_STREAM), 
-	//							and the TCP/IP protocol (IPPROTO_TCP).
     SOCKET listenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	// Check for errors to ensure that the socket is a valid socket.
-	// Error detection is a key part of successful networking code. 
-	// If the socket call fails, it returns INVALID_SOCKET. 
-	// The if statement in the previous code is used to catch any errors that
-	// may have occurred while creating the socket. WSAGetLastError returns an 
-	// error number associated with the last error that occurred.
 	if (INVALID_SOCKET == listenSocket)
 	{
         cout<<"Http Server: Error at socket(): "<<WSAGetLastError()<<endl;
@@ -70,32 +47,13 @@ void main()
         return;
 	}
 
-	// For a server to communicate on a network, it must bind the socket to 
-	// a network address.
-
-	// Need to assemble the required data for connection in sockaddr structure.
-
     // Create a sockaddr_in object called serverService. 
 	sockaddr_in serverService;
-	// Address family (must be AF_INET - Internet address family).
     serverService.sin_family = AF_INET; 
-	// IP address. The sin_addr is a union (s_addr is a unsigned long 
-	// (4 bytes) data type).
-	// inet_addr (Iternet address) is used to convert a string (char *) 
-	// into unsigned long.
+	
 	// The IP address is INADDR_ANY to accept connections on all interfaces.
 	serverService.sin_addr.s_addr = INADDR_ANY;
-	// IP Port. The htons (host to network - short) function converts an
-	// unsigned short from host to TCP/IP network byte order 
-	// (which is big-endian).
 	serverService.sin_port = htons(HTTP_PORT);
-
-	// Bind the socket for client's requests.
-
-    // The bind function establishes a connection to a specified socket.
-	// The function uses the socket handler, the sockaddr structure (which
-	// defines properties of the desired connection) and the length of the
-	// sockaddr structure (in bytes).
     if (SOCKET_ERROR == bind(listenSocket, (SOCKADDR *) &serverService, sizeof(serverService))) 
 	{
 		cout<<"Http Server: Error at bind(): "<<WSAGetLastError()<<endl;
@@ -105,8 +63,6 @@ void main()
     }
 
     // Listen on the Socket for incoming connections.
-	// This socket accepts only one connection (no pending connections 
-	// from other clients). This sets the backlog parameter.
     if (SOCKET_ERROR == listen(listenSocket, 5))
 	{
 		cout << "Http Server: Error at listen(): " << WSAGetLastError() << endl;
@@ -121,14 +77,9 @@ void main()
     // Accept connections and handles them one by one.
 	while (true)
 	{
-		// The select function determines the status of one or more sockets,
-		// waiting if necessary, to perform asynchronous I/O. Use fd_sets for
-		// sets of handles for reading, writing and exceptions. select gets "timeout" for waiting
-		// and still performing other operations (Use NULL for blocking). Finally,
-		// select returns the number of descriptors which are ready for use (use FD_ISSET
-		// macro to check which descriptor in each set is ready to be used).
 		fd_set waitRecv;
 		FD_ZERO(&waitRecv);
+
 		for (int i = 0; i < MAX_SOCKETS; i++)
 		{
 			if ((sockets[i].recv == LISTEN) || (sockets[i].recv == RECEIVE))
@@ -137,17 +88,13 @@ void main()
 
 		fd_set waitSend;
 		FD_ZERO(&waitSend);
+
 		for (int i = 0; i < MAX_SOCKETS; i++)
 		{
 			if (sockets[i].send == SEND)
 				FD_SET(sockets[i].id, &waitSend);
 		}
 
-		//
-		// Wait for interesting event.
-		// Note: First argument is ignored. The fourth is for exceptions.
-		// And as written above the last is a timeout, hence we are blocked if nothing happens.
-		//
 		int nfd;
 		nfd = select(0, &waitRecv, &waitSend, NULL, NULL);
 		if (nfd == SOCKET_ERROR)
@@ -157,6 +104,7 @@ void main()
 			return;
 		}
 
+		//handle incoming requests
 		for (int i = 0; i < MAX_SOCKETS && nfd > 0; i++)
 		{
 			
@@ -181,6 +129,7 @@ void main()
 
 		}
 
+		//process requests, prepeare and send responses
 		for (int i = 0; i < MAX_SOCKETS && nfd > 0; i++)
 		{
 			if (FD_ISSET(sockets[i].id, &waitSend))
